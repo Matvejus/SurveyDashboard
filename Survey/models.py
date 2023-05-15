6c4eb37bfeda256765dcc771fafeca29deebdef4
Haley-ng <103782566+Haley-ng@users.noreply.github.com>
2023-05-15 13:02:45 +0200

    Update models.py

--- Survey/models.py
+++ Survey/models.py
@@ -1,126 +1,111 @@
 from django.db import models
-import datetime
 
-# Models for Survey
+# Create your models here.
+# Step 1 - Define models
+
+
+class Organisation(models.Model):
+    org_id = models.AutoField(primary_key=True)
+    organisation = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = "Organisation"
 
 
 class SurveyStatus(models.Model):
-    survey_status_id = models.AutoField(primary_key=True)
-    survey_status = models.CharField(max_length=100, blank=True, null=True)
+    status_id = models.AutoField(primary_key=True)
+    status = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = "Survey_Status"
 
     def __str__(self):
-        return f"{self.survey_status}"
+        return self.status
 
 
 class Survey(models.Model):
     survey_id = models.AutoField(primary_key=True)
-    survey_name = models.CharField(max_length=100)
+    status = models.ForeignKey("SurveyStatus", models.DO_NOTHING)
+    title = models.CharField(max_length=255)
     description = models.CharField(max_length=8000)
+    org = models.ForeignKey(Organisation, models.DO_NOTHING)
     start_date = models.DateTimeField(blank=True, null=True)
     end_date = models.DateTimeField(blank=True, null=True)
-    min_responses = models.IntegerField(blank=True, null=True)
-    max_responses = models.IntegerField(blank=True, null=True)
-    survey_status = models.ForeignKey("SurveyStatus", models.DO_NOTHING)
 
     class Meta:
-        verbose_name_plural = 'Surveys'
         managed = False
         db_table = "Survey"
 
     def __str__(self):
-        return f"{self.survey_name}"
+        return self.title
 
 
 class QuestionType(models.Model):
-    question_type_id = models.AutoField(primary_key=True)
-    question_type = models.CharField(max_length=100, blank=True, null=True)
+    type_id = models.AutoField(primary_key=True)
+    text = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = "Question_Type"
 
     def __str__(self):
-        return f"{self.question_type}"
+        return self.text
 
 
 class Question(models.Model):
     question_id = models.AutoField(primary_key=True)
-    question_order = models.IntegerField(blank=True, null=True)
-    question_text = models.CharField(max_length=2000, blank=True, null=True)
-    is_mandatory = models.TextField(
-        blank=True, null=True
-    )  # This field type is a guess.
-    question_type = models.ForeignKey("QuestionType", models.DO_NOTHING)
+    type = models.ForeignKey("QuestionType", models.DO_NOTHING)
+    survey = models.ForeignKey(Survey, on_delete=models.PROTECT)
+    text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
 
     class Meta:
         managed = False
         db_table = "Question"
 
     def __str__(self):
-        return f"{self.question_text}"
+        return self.text
 
 
-class QuestionOption(models.Model):
-    question_option_id = models.AutoField(primary_key=True)
-    qo_order = models.IntegerField(
-        db_column="QO_order", blank=True, null=True
-    )  # Field name made lowercase.
-    qo_value = models.CharField(
-        db_column="QO_value", max_length=100, blank=True, null=True
-    )  # Field name made lowercase.
-    question = models.ForeignKey(Question, models.DO_NOTHING)
+class Choice(models.Model):
+    choice_id = models.AutoField(primary_key=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
 
     class Meta:
         managed = False
-        db_table = "Question_Option"
-
-
-class Respondent(models.Model):
-    respondent_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100, blank=True, null=True)
-    last_name = models.CharField(max_length=100, blank=True, null=True)
-    email = models.CharField(max_length=100, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = "Respondent"
-
-
-class Response(models.Model):
-    response_id = models.AutoField(primary_key=True)
-    respondent = models.ForeignKey(Respondent, models.DO_NOTHING)
-    survey = models.ForeignKey("Survey", models.DO_NOTHING)
-    begin_date = models.DateTimeField(blank=True, null=True)
-    end_date = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = "Response"
-
-
-class Answer(models.Model):
-    answer_id = models.AutoField(primary_key=True)
-    response = models.ForeignKey("Response", models.DO_NOTHING)
-    question = models.ForeignKey("Question", models.DO_NOTHING)
-    answer = models.CharField(max_length=6000, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = "Answer"
+        db_table = "Choice"
 
     def __str__(self):
-        return f"{self.answer}"
+        return f"{self.question.text}:{self.text}"
 
 
-class AnswerOption(models.Model):
-    answer_option_id = models.AutoField(primary_key=True)
-    answer = models.ForeignKey(Answer, models.DO_NOTHING)
-    question_option = models.ForeignKey("QuestionOption", models.DO_NOTHING)
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
+    org = models.ForeignKey(Organisation, models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = "Answer_Option"
+        db_table = "User"
+
+
+class Submission(models.Model):
+    submission_id = models.AutoField(primary_key=True)
+    survey = models.ForeignKey(Survey, on_delete=models.PROTECT)
+    user = models.ForeignKey("User", models.DO_NOTHING)
+    answer = models.ManyToManyField(Choice)
+    sub_date = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = "Submission"
+
+
+# Step 1a - Create migrations
+
+# Step 1b - Run the migrations
