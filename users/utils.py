fe14400e75807a895ebb7bf43c53d8e7bb6d80c4
thomas237 <tdavoost@hotmail.nl>
2023-06-06 13:47:59 +0200

    roles

---
+++ users/utils.py
@@ -0,0 +1,77 @@
+from django.contrib.auth.models import Group, Permission
+
+GROUPS_PERMISSIONS = {
+    'Collaborator': [
+        'view_answer',
+        'view_question',
+        'view_survey',
+        'add_useranswer',
+        'view_useranswer',
+        'view_collaborationnetwork',
+        'view_orgprofile',
+        'change_customuser',
+        'delete_customuser',
+        'view_customuser',
+    ],
+
+    'Orchestrator': [
+    'add_answer',
+    'change_answer',
+    'delete_answer',
+    'view_answer',
+    'add_question',
+    'change_question',
+    'view_question',
+    'view_survey',
+    'add_useranswer',
+    'view_useranswer',
+    'view_collaborationnetwork',
+    'view_orgprofile',
+    'change_customuser',
+    'delete_customuser',
+    'view_customuser',
+    ],
+
+    'Supervisor': [
+    'add_answer',
+    'change_answer',
+    'delete_answer',
+    'view_answer',
+    'add_question',
+    'change_question',
+    'delete_question',
+    'view_question',
+    'add_survey',
+    'change_survey',
+    'delete_survey',
+    'view_survey',
+    'add_useranswer',
+    'view_useranswer',
+    'add_collaborationnetwork',
+    'change_collaborationnetwork',
+    'delete_collaborationnetwork',
+    'view_collaborationnetwork',
+    'add_orgprofile',
+    'change_orgprofile',
+    'delete_orgprofile',
+    'view_orgprofile',
+    'change_customuser',
+    'delete_customuser',
+    'view_customuser',
+]
+,
+}
+
+def create_group_permissions(name, permission_codenames):
+    group, created = Group.objects.get_or_create(name=name)
+    permissions = Permission.objects.filter(codename__in=permission_codenames)
+    group.permissions.set(permissions)
+    return group
+
+def setup_groups_permissions(user, default_group='Collaborator'):
+    for group_name, permission_codenames in GROUPS_PERMISSIONS.items():
+        group = create_group_permissions(group_name, permission_codenames)
+        if group_name == default_group:
+            user.groups.add(group)
+
+    return user
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
+from django.views import View
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView
+from django.utils.decorators import method_decorator
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import user_passes_test, login_required
 from .forms import CustomUserCreationForm
 
 class RegisterView(CreateView):
@@ -9,14 +13,23 @@ class RegisterView(CreateView):
     success_url = reverse_lazy("organization:index")
     template_name = "registration/registration.html"
 
-def update_user_profile(request):
-    if request.method == 'POST':
+def group_required(group_name):
+    def check_group(user):
+        group = Group.objects.get(name=group_name)
+        return user.groups.filter(name=group_name).exists()
+
+    return user_passes_test(check_group)
+
+@method_decorator([login_required, group_required('collaborator')], name='dispatch')
+class update_user_profile(View):
+    def post(self, request):
         form = CustomUserCreationForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
             update_session_auth_hash(request, request.user)
             return redirect(reverse('organization:profile'))  # Redirect and Reverse to the profile page
-    else:
-        form = CustomUserCreationForm(instance=request.user)
 
-    return render(request, 'user_profiles/edit_profile.html', {'form': form})
+    def get(self, request):
+        form = CustomUserCreationForm(instance=request.user)
+        return render(request, 'user_profiles/edit_profile.html', {'form': form})
+
