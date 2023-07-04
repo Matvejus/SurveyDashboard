42c449d1970bfa0b3d96afe8482a9b8a203e861a
thomas237 <tdavoost@hotmail.nl>
2023-07-04 17:56:44 +0200

    correct admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .forms import CustomUserCreationForm
 from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
@@ -12,5 +13,21 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ("organization",)
     search_fields = ("email", "first_name", "last_name", "organization__title")
     ordering = ("organization", "last_name")
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'username', 'organization', 'collaboration_network', 'position', 'avatar', 'password1', 'password2'),
+        }),
+    )
+
+    fieldsets = (
+        (_('User'), {'fields': ('email', 'username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'organization', 'collaboration_network', 'position', 'avatar')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
